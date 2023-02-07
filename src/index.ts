486da1e9e033023e72120812f8d81747ed683021
Alex Currie-Clark <acurrieclark@gmail.com>
2023-02-07 10:41:43 +0000

    Remove unnecessary imports

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,2 @@
-import { Prop } from "@automerge/automerge";
-
 export { unpatch } from "./unpatch";
 export { patch } from "./patch";
-
-type Value = string | number | boolean | null | Date | Uint8Array;
